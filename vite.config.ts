214366745104d9b14a26d796e2db0a1891a58a0f
ilya <pasterzoom@gmail.com>
2023-06-25 10:34:47 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: 'NonExistingPath',
-    },
-  },
+  base: '/src/',
 })
